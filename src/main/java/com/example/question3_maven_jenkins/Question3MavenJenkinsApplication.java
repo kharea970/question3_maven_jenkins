78580e2c1bd8c651f941642ffbe9eafe486d4c99
Ashish Khare <kharea970@gmail.com>
2022-11-12 15:11:44 +0530

    initial commit

---
+++ src/main/java/com/example/question3_maven_jenkins/Question3MavenJenkinsApplication.java
@@ -0,0 +1,15 @@
+package com.example.question3_maven_jenkins;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Question3MavenJenkinsApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(Question3MavenJenkinsApplication.class, args);
+		System.out.println("hello world");
+	}
+
+}
---
+++ src/test/java/com/example/question3_maven_jenkins/Question3MavenJenkinsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.question3_maven_jenkins;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Question3MavenJenkinsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
